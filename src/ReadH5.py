057d3ebf73df58b8d1f5d7b5e123b8523002c426
John Reynolds <john@Johns-iMac-2.fios-router.home>
2019-04-13 12:33:14 -0400

    prevent reads on already loaded df

--- src/ReadH5.py
+++ src/ReadH5.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import datetime
 import pytz
+import time
 
 class ReadH5 :
 
@@ -10,10 +11,10 @@ class ReadH5 :
         self.dataDir = dataDir
         self.tz = pytz.timezone(timeZone)
         self.reportNoData = reportNoData
+        self.df = None
 
     def findFile(self, symbol, date):
         def getdt(d,tz=self.tz) :
-            #return tz.localize(datetime.datetime(int(d[0:4]), int(d[4:6]), int(d[6:8]),0,0,0))
             return datetime.datetime(int(d[0:4]), int(d[4:6]), int(d[6:8]),0,0,0)
 
         for f in os.listdir(self.dataDir):
@@ -24,12 +25,24 @@ class ReadH5 :
             
 
     def readh5(self, symbol, date):
+        if self.df is not None:
+            if date > self.start_date and date < self.end_date :
+                return self.df
         fname = self.findFile(symbol, date)
         if fname is None:
+            self.df = None
             if self.reportNoData :
                 print("no data for this symbol:date %s:%s" % (symbol, date))
             return None
-        df =pd.read_hdf(os.path.join(self.dataDir, fname), 'table')
-        #df.set_index('dt', inplace=True)
-        return df
+        t0 = time.time()
+        self.df =pd.read_hdf(os.path.join(self.dataDir, fname), 'table')
+        self.start_date, self.end_date = self.df['dt'].min(), self.df['dt'].max()
+        t1 = time.time()
+        if self.reportNoData :
+            print("reading time for %s %f" % (fname, (t1-t0)))
+        return self.df
         
+
+    def setIndex(self):
+        # Not used for now
+        self.df.set_index('dt', inplace=True)
