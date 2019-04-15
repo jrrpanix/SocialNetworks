53f83d241341b92ba206733823273f67c3edf5cc
JOHN REYNOLDS <jrr@panix.com>
2019-04-15 15:05:43 -0400

    eval

--- src/EvalDates.py
+++ src/EvalDates.py
@@ -10,38 +10,43 @@ from Utils import Utils
 
 class EvalDates:
 
-    def __init__(self, dataDir, dates, event, symbol, before, after, outdir):
+    def __init__(self, dataDir, dates, event, symbol, before, after, outdir, pre):
         self.reader = ReadH5(dataDir, reportNoData=False)
         self.dates = dates
         self.event = event
         self.symbol = symbol
         self.before = before
         self.after = after
+        self.pre = pre
         self.outdir = outdir
         self.TickSize = Utils.tickSize(symbol)
-        self.outstr = "{}_{}_{}_{}".format(symbol, before, after, event)
+        self.TickValue = Utils.tickValue(symbol)
+        self.outstr = "{}_{}_{}_{}_pre_{}".format(symbol, before, after, event, pre)
         self.run()
         print("Eval Complete output file %s" % self.outfile)
 
     def run(self):
         self.outfile = os.path.join(self.outdir, "{}.{}".format(self.outstr,"csv"))
         fd=open(self.outfile, "w")
-        header="date,beginD,endD,O,H,L,C,R,T,V,N"
+        header="date,beginD,endD,O,H,L,C,R,T,V,N,$C,pre_date,pre_R,pre_T,pre_V"
         fd.write( "%s\n" % header)
         for date in self.dates:
             tickdf = self.reader.readh5(self.symbol, date)
             if tickdf is not None:
+                preT = Utils.todt(date) - datetime.timedelta(seconds=pre)
+                p = ComputeOHLC.calc(tickdf, preT, self.before, self.after, self.TickSize)
                 o = ComputeOHLC.calc(tickdf, date, self.before, self.after, self.TickSize)
-                fd.write("%s,%s,%s%f,%f,%f,%f,%f,%f,%f,%f\n" %
-                         (o.D, o.beginD, o.endD, o.O, o.H, o.L, o.C, o.R, o.Ticks, o.V, o.N))
+                cDollars = ((o.H - o.L)/self.TickSize)*self.TickValue
+                fd.write("%s,%s,%s%f,%f,%f,%f,%f,%f,%f,%f,%f,%s,%f,%f,%f\n" %
+                         (o.D, o.beginD, o.endD, o.O, o.H, o.L, o.C, o.R, o.Ticks, o.V, o.N, cDollars, p.D, p.R, p.T, p.V))
         fd.close()
 
-def RunEvent(dataDir, ann, event, symbol, before, after, outdir) :
+def RunEvent(dataDir, ann, event, symbol, before, after, outdir, pre) :
     eventdf = ann[ann["event"]==event] if event is not None else None
     if len(eventdf) == 0 : print("no data for event %s" % event)
     else:
         print("Running Event %s" % event)
-        EvalDates(dataDir, eventdf.dt.values, event, symbol, before, after, outdir)
+        EvalDates(dataDir, eventdf.dt.values, event, symbol, before, after, outdir, pre)
 
 if __name__ == "__main__":
     # Note the CME Futures Data Timestamps are in Central Time
@@ -50,6 +55,7 @@ if __name__ == "__main__":
     parser.add_argument('-s','--symbol', help='symbol ', default="ES")
     parser.add_argument('-b','--before', help='before window seconds ', default=60, type=int)
     parser.add_argument('-a','--after', help='after window seconds', default=60*10, type=int)
+    parser.add_argument('-p','--pre', help='pre time return', default=60*60, type=int)
     parser.add_argument('-o','--outdir', help='output dir', default=None)
     parser.add_argument('-i','--inputs', help='input file of dates', default="../data/dailyfx.h5")
     parser.add_argument('-e','--event', help="event or file of eventsnames", default=None)
@@ -58,7 +64,7 @@ if __name__ == "__main__":
     parser.add_argument('-l','--list', help='list events', action='store_true', default = False)
     args = parser.parse_args()
 
-    inputs, dataDir, symbol, before, after, event, outdir = args.inputs, args.dataDir, args.symbol, args.before, args.after, args.event, args.outdir
+    inputs, dataDir, symbol, before, after, event, outdir, pre = args.inputs, args.dataDir, args.symbol, args.before, args.after, args.event, args.outdir, args.pre
     assert os.path.exists(dataDir), "tick data directory %s does not exist" % dataDir
     assert os.path.exists(inputs), "announcement file %s does not exist" % inputs
     assert os.path.exists(outdir), "output directory %s does not exist" % outdir
@@ -76,8 +82,8 @@ if __name__ == "__main__":
                     e = line.split(",")[0].strip()
                     # some 
                     if len(e.split("/")) > 1 : continue #event names with / cause problemes
-                    RunEvent(dataDir, ann, e, symbol, before, after, outdir)
+                    RunEvent(dataDir, ann, e, symbol, before, after, outdir, pre)
         else:
-            RunEvent(dataDir, ann, event, symbol, before, after, outdir)
+            RunEvent(dataDir, ann, event, symbol, before, after, outdir, pre)
 
     
